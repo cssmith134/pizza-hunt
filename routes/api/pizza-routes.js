2996bc4069e1e5e59d739f3ca863ccf84254905c
REX <cssmith431@yahoo.com>
2022-04-14 14:27:50 -0400

    comments

--- routes/api/pizza-routes.js
+++ routes/api/pizza-routes.js
@@ -9,17 +9,17 @@ const {
   } = require('../../controllers/pizza-controller');
   
 
-// Set up GET all and POST at /api/pizzas
+
 router
   .route('/')
-  .get()
-  .post();
+  .get(getAllPizza)
+  .post(createPizza);
 
-// Set up GET one, PUT, and DELETE at /api/pizzas/:id
+
 router
   .route('/:id')
-  .get()
-  .put()
-  .delete();
+  .get(getPizzaById)
+  .put(updatePizza)
+  .delete(deletePizza);
 
 module.exports = router;
